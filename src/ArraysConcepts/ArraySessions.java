f4aa0a703d9f5e146c187a6c6527ac33c5ea11c0
Krishnarjun123 <kk.arjun92@gmail.com>
2022-07-04 18:09:19 +0530

    adding arrays

--- src/ArraysConcepts/ArraySessions.java
+++ src/ArraysConcepts/ArraySessions.java
@@ -1,4 +1,34 @@
 package ArraysConcepts;
 
 public class ArraySessions {
-}
+   static int a[]={10,20,30,40,50,60};
+
+
+    String names[]={"naveen","poorna","krishna"};
+    boolean c[]={true,false};
+  static   int b[]=new int[6];
+
+    public static void main(String[] args) {
+        b[0]=10;
+        b[1]=20;
+        b[2]=30;
+        b[3]=40;
+        b[4]=50;
+        b[5]=60;
+        System.out.println(a.length);
+        System.out.println(b);
+        System.out.println(b[0]);
+        System.out.println(b[1]);
+        System.out.println(b[2]);
+        System.out.println(b[3]);
+        System.out.println(b[4]);
+        System.out.println(b[5]);
+
+
+    }
+
+
+    }
+
+
+
