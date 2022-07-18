74b9167540988df1084f27e4fc6ad489b77ba661
Krishnarjun123 <kk.arjun92@gmail.com>
2022-07-18 18:51:10 +0530

    StringExamples

--- src/StringSessions/StringExamples.java
+++ src/StringSessions/StringExamples.java
@@ -26,5 +26,40 @@ public class StringExamples {
         fullName =firstName.concat(lastName);
         System.out.println(fullName);
         System.out.println("full name of the string is:"+fullName);
+
+        /*String s1="My name is Naveen";
+        String s2="my name is naveen";
+        System.out.println(s1==s2);
+        String s3=new String("My Name is Naveen");
+        String s4=new String("my name is naveen");
+        System.out.println(s3==s4);
+        System.out.println(s3.equals(s4));
+        System.out.println(s3.equalsIgnoreCase(s4));
+        System.out.println(s1.indexOf("Naveen"));*/
+
+        /*String name="purnachandhu";
+        char[]  ch=name.toCharArray();
+        System.out.println(ch.length);
+        System.out.println(ch[0]);
+        System.out.println(ch[ch.length-1]);*/
+
+
+       /* String name ="I Love My India";
+
+        String [] splittedstring=name.split(" ");
+        */
+
+        String name="I Love My India";
+
+        String[] splittedstring=name.split(" ");
+        System.out.println(splittedstring[0]);
+        System.out.println(splittedstring[1]);
+        System.out.println(splittedstring[3]);
+        System.out.println(splittedstring[2]);
+
+
+        String name1="I Love My India";
+        String[] splittedstring1=name1.split(" ");
+
     }
 }
