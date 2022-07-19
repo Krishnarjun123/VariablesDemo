d44263b685c1051b85129d9fc449c67478a549f3
Krishnarjun123 <kk.arjun92@gmail.com>
2022-07-20 00:27:43 +0530

    MethodOverloading

---
+++ src/OOPSPolymorphisam/Mainmethod.java
@@ -0,0 +1,11 @@
+package OOPSPolymorphisam;
+
+public class Mainmethod {
+   static MethodOverloading obj=new MethodOverloading(10,20);
+
+    public static void main(String[] args) {
+        obj.add(10,20);
+        obj.add(20,10,30);
+
+    }
+}
