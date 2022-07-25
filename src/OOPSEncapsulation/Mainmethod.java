27b8d403f13774260bb161fe31fd434519a0e860
Krishnarjun123 <kk.arjun92@gmail.com>
2022-07-25 21:54:40 +0530

    EncapsulationEx

---
+++ src/OOPSEncapsulation/Mainmethod.java
@@ -0,0 +1,10 @@
+package OOPSEncapsulation;
+
+public class Mainmethod {
+    public static void main(String[] args) {
+        EncapsulationEx obj=new EncapsulationEx();
+        obj.setName("Phani");
+        System.out.println(obj.getName());
+    }
+
+}
