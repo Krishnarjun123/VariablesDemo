5e99b0c0140370107ed5b9d38812777a899cca96
Krishnarjun123 <kk.arjun92@gmail.com>
2022-07-27 19:41:40 +0530

    InterfaceEx

---
+++ src/Interface/InterfaceParent.java
@@ -0,0 +1,6 @@
+package Interface;
+
+public interface InterfaceParent {
+    public void add();
+    int a=10;
+}
---
+++ src/Interface/Mainmethod.java
@@ -0,0 +1,9 @@
+package Interface;
+
+public class Mainmethod {
+    public static void main(String[] args) {
+        InterfaceParent obj=new InterfaceChild();
+        obj.add();
+        B obj1=new C();
+    }
+}
