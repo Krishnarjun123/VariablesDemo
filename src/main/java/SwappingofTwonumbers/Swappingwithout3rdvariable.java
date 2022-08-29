2088698aa99e0469cbb339ddd5a1ea6ec93fcc62
Krishnarjun123 <kk.arjun92@gmail.com>
2022-08-29 16:19:52 +0530

    Swapingwithout3rdvariabe.

---
+++ src/main/java/SwappingofTwonumbers/Swappingwithout3rdvariable.java
@@ -0,0 +1,16 @@
+package SwappingofTwonumbers;
+
+public class Swappingwithout3rdvariable {
+    int a=10;
+    int b=20;
+    //      a=a+b;
+    //      b=a-b;
+    //      a=a-b;
+
+
+
+    //SWAPPING OF TWO NUMBERS WITH 3RD VARIABLE;
+    //     temp=a;
+    //     a=b;
+    //     b=temp;
+}
